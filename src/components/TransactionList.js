65de78d4f032d9b90b2815c72221697d58415785
Staff Asia <staffasia@Staffs-iMac.local>
2022-03-02 14:23:22 +0600

    adding transactions data to ui

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 const TransactionList = () => {
+    const context = useContext(GlobalContext);
+    const {transactions} = context;
+
     return (
         <>
             <h3>History</h3>
-            <ul  className="list">
-                <li className="minus">
-                Cash <span>-$400</span><button className="delete-btn">x</button>
-                </li>
+            <ul className="list">
+                {transactions.map(transaction => (
+                    <li className="minus" key={transaction.id}>
+                    {transaction.text} <span>-$400</span><button className="delete-btn">x</button>
+                    </li>
+                ))}
             </ul>
         </>
     );
